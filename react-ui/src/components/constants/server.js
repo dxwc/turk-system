09b636cd39fb7c73b19d42647c04a7d6522a9602
Ted Shin <tdyshin@gmail.com>
2017-10-15 17:16:11 -0400

    Set up constants for heroku deployment

--- react-ui/src/components/constants/server.js
+++ react-ui/src/components/constants/server.js
@@ -1,6 +1,6 @@
 // Server URL
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://myapidomain.com'
+  ? 'https://turk-system.herokuapp.com/stripe'
   : 'http://localhost:5000/stripe';
 
 export default PAYMENT_SERVER_URL;
--- server/constants/frontend.js
+++ server/constants/frontend.js
@@ -1,9 +1,9 @@
 // These URLs are used to create whitelist for CORS
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ];
-
+// https://turk-system.herokuapp.com/
 const FRONTEND_PROD_URLS = [
-  'https://www.yourdomain.com',
-  'https://yourdomain.com'
+  'https://www.herokuapp.com/3000',
+  'https://turk-system.herokuapp.com/3000'
 ];
 
 module.exports = process.env.NODE_ENV === 'production'
