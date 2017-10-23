aa8e0baacd568b2edeb2f9254f96620f0b322f3f
Ted Shin <tdyshin@gmail.com>
2017-10-23 19:25:00 -0400

    Set up user applications page that only shows for logged in super users

--- application/server/routes/index.js
+++ application/server/routes/index.js
@@ -8,6 +8,7 @@ const logout = require('./logout');
 const profile = require('./profile');
 const pageNotFound = require('./404');
 const charge = require('./charge');
+const userApps = require('./userApps');
 
 const stripeExample = require('./stripeExample');
 const paymentApi = require('./payment');
@@ -17,10 +18,21 @@ const testApi = require('./test');
 const isLoggedIn = (req, res, next) => {
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
-      return next();
+    return next();
+
+  // if they aren't redirect them to welcome page
+  res.redirect('/');
+}
+
+// route middleware to make sure user is superuser
+const isSuperuser = (req, res, next) => {
+  // If user is superuser, carry on
+  if (req.user.local.usertype === 'superuser') {
+    return next();
+  }
 
   // if they aren't redirect them to the home page
-  res.redirect('/');
+  res.redirect('/home');
 }
 
 const configureRoutes = (app, passport) => {
@@ -31,6 +43,7 @@ const configureRoutes = (app, passport) => {
   logout(app);
   profile(app, isLoggedIn);
   charge(app);
+  userApps(app, isSuperuser);
 
   stripeExample(app);
   paymentApi(app);
--- application/server/routes/signup.js
+++ application/server/routes/signup.js
@@ -7,7 +7,7 @@ const signup = (app, passport) => {
 
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
+    successRedirect : '/home', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
