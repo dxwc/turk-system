ceccdc0359eb3ac3149b97adf9f80efe195e2437
dxwc <dxwc@users.noreply.github.com>
2017-10-28 00:42:26 -0400

    Added express.js

--- application/index.js
+++ application/index.js
@@ -1,1 +1,13 @@
-// Starting point for the application
+// Starting point for the whole application
+
+const express = require('express');
+
+/** http://expressjs.com/en/4x/api.html */
+const app = express();
+
+/** Server listen for requests on port 9001 or if any port specified in environmental
+ * variable PORT */
+const server = app.listen('9001' || process.env.PORT, () =>
+{
+    console.log('Listening on', server.address());
+});
