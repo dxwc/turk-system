92bd2a96fad460c18e55d75761f1d1974ee7ba3f
dxwc <dxwc@users.noreply.github.com>
2017-11-28 21:55:09 -0500

    init v2

---
+++ dev_docs/README.md
@@ -0,0 +1,21 @@
+## Mongodb setup instruction for most linux:
+
+## Installation setup
+
++ download and install from distro's repository
++ `sudo systemctl enable mongodb`
+
+## Initialization/use
+
++ `sudo systemctl start mongodb` -- start db server
++ `sudo systemctl stop mongodb` -- stop db server
+
+# Module documentation links
+
++ Express.js : <http://expressjs.com/en/4x/api.html>
++ EJS : <http://ejs.co/>
++ Axios : <https://github.com/axios/axios>
++ Mongoose: <http://mongoosejs.com>
++ Body-parser : <https://github.com/expressjs/body-parser>
++ Express-session : <https://www.npmjs.com/package/express-session>
++ Connect-mongo : <https://www.npmjs.com/package/connect-mongo>
---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+ * Starting point for the application
+ * Mongodb: port 27017, database name: turk_system
+ * Express server: port 9001
+ */
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise; // use ES6 promise for mongoose promise
+require('./models'); // defines all the data model (schema)
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const express = require('express');
+const app = express();
+
+require('ejs').delimiter = '?';
+/** Set view engine to ejs and set directory to look up ejs file to ./views/ */
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+/** Serves requested files if found in ./static_files folder */
+app.use(express.static(__dirname + '/static_files'));
+
+/** session middleware to save data to validate user */
+app.use(session({
+    secret: 'foo',
+    store: new MongoStore({ mongooseConnection : mongoose.connection }),
+    // using existing mongodb connection
+    ttl: .50 * 60 * 60, // session will exire in half an hour
+    resave: true,
+    saveUninitialized: true
+}));
+
+// ** A middleware to have access to POST sent data (ex: req.body.some_data) : **/
+// For content type : application/x-www-form-urlencoded (default for all POST or GET)
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(require('body-parser').json()); // For parsing application/json content type
+
+//>>>>>---Routes--->>>>>
+app.use(require('./routes/404')); // should be last one in order of routes
+//<<<<<---Routes---<<<<<
+
+// Connect mongodb on localhost at port 27017 on database turk_system
+mongoose.connect('mongodb://localhost:27017/turk_system', { useMongoClient: true })
+.then((db) =>
+{
+    console.log('Connected to MongoDB server on port: 27017, databse: turk_system');
+
+    /** Server listen for requests on port 9001 or if any port specified in
+     * environmental variable PORT */
+    const server = app.listen('9001' || process.env.PORT, () =>
+    {
+        console.log('Express server started listening on', server.address());
+    });
+})
+.catch((err) =>
+{
+    console.log('Mongo DB connection Error, server will not attemp starting', err);
+});
