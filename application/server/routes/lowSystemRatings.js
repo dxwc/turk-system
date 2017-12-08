729cc68fa716c683931e540cf5aab891e7ee83f9
Ted Shin <tdyshin@gmail.com>
2017-12-08 08:49:31 -0500

    Add input fields for admin to give money to dev, give back to client, set new rating, justification for low rated system demands

--- application/server/routes/lowSystemRatings.js
+++ application/server/routes/lowSystemRatings.js
@@ -1,5 +1,6 @@
 let User = require('../models/user.js');
 let Demand = require('../models/demand.js');
+let Rating = require('../models/rating.js');
 
 const lowSystemRatings = (app, isLoggedIn, checkUserAccess) => {
 
@@ -9,9 +10,24 @@ const lowSystemRatings = (app, isLoggedIn, checkUserAccess) => {
     });
   };
 
+  const getLowSystemRatings = (req, res) => {
+    Rating
+      .find({ 'rating': { $lt: 3 } } )
+      .exec(function(err, ratings) {
+        if (err) {
+          throw err;
+        } else if (ratings) {
+          console.log(ratings);
+          res.json(ratings);
+        } else {
+          res.send('No low ratings found');
+        }
+      });
+  };
+
 
   app.get('/low-system-ratings', isLoggedIn, checkUserAccess, renderlowSystemRatings);
-
+  app.get('/api/low-ratings', isLoggedIn, checkUserAccess, getLowSystemRatings);
 
   return app;
 }
