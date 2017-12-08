d03267440584a3a79b709ee5b0438aa4ccf62957
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-12-07 19:46:12 -0500

    Query number of devs and clients into grand sys stats

--- application/server/routes/grand_stats.js
+++ application/server/routes/grand_stats.js
@@ -1,11 +1,31 @@
+let User = require('../models/user.js');
+
 const grandstats = (app, isLoggedIn) => {
-  // we will want this protected so you have to be logged in to visit
-  // we will use route middleware to verify this (the isLoggedIn function)
-  app.get('/grandstats', isLoggedIn, function(req, res) {
-    res.render('grandstats.ejs', {
-      user: req.user // get the user out of session and pass to template
-    });
-  });
+
+  let numberOfClients = 0;
+  let numberOfDeveloprs = 0;	
+  const getNumberOfClients = (req, res) => {
+  	User
+  	  .count({ 'local.usertype': 'client' })
+  	  .exec(function(err, clientCount) {
+  	  	if (err) { throw err; }
+  	  	numberOfClients = clientCount;
+  	  	// console.log(numberOfClients);
+  	  	User
+  	  	  .count({ 'local.usertype': 'developer' })
+  	  	  .exec(function(err2, devCount) {
+  	  	  	if (err2) { throw err2; }
+  	  	  	numberOfDevelopers = devCount;
+  	  	  	res.render('grandstats.ejs', {
+	      	  user: req.user,
+	      	  clients: numberOfClients,
+	      	  developers: numberOfDevelopers
+    		});
+  	  	})
+  	}) 
+  };
+
+  app.get('/grandstats', isLoggedIn, getNumberOfClients);
 
   return app;
 }
